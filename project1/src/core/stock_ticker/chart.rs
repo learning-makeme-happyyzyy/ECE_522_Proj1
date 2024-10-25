1b55fd5037bc4fada08bf6c2879b52bf3c432b96
learning-makeme-happyyzyy <zhouyiya@ualberta.ca>
2024-10-25 12:55:49 -0600

    Update chart.rs

--- project1/src/core/stock_ticker/chart.rs
+++ project1/src/core/stock_ticker/chart.rs
@@ -2,7 +2,7 @@ use yahoo_finance_api::Quote;
 use chrono::NaiveDateTime;
 use plotters::prelude::*;
 
-pub fn print_closing_prices_and_dates(quotes: &Vec<Quote>, volatile_days: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+pub fn print_closing_prices_and_dates(quotes: &Vec<Quote>, volatile_days: &Vec<String>, stock_symbol:&str) -> Result<(), Box<dyn std::error::Error>> {
     let closing_prices: Vec<f64> = quotes.iter().map(|q| q.close).collect();
     let high: Vec<f64> = quotes.iter().map(|q| q.high).collect();
     let low: Vec<f64> = quotes.iter().map(|q| q.low).collect();
@@ -16,6 +16,8 @@ pub fn print_closing_prices_and_dates(quotes: &Vec<Quote>, volatile_days: &Vec<S
     
     // println!("{:?}", positions);
     
+    let min_price = closing_prices.iter().cloned().fold(f64::INFINITY, f64::min);
+    let max_price = closing_prices.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
 
     // Convert Vec<String> to Vec<&str>
     let date_strs: Vec<&str> = dates.iter().map(|s| s.as_str()).collect();
@@ -28,11 +30,11 @@ pub fn print_closing_prices_and_dates(quotes: &Vec<Quote>, volatile_days: &Vec<S
 
     // let ymax = closing_prices.iter().cloned().fold(f64::NAN, f64::max);
 
-    plot(&date_strs, &closing_prices, &high, &low, &positions)?;
+    plot(&date_strs, &closing_prices, &high, &low, &positions, min_price, max_price, stock_symbol)?;
     Ok(())
 }
 
-fn plot(dates: &[&str], closing_prices: &[f64], high: &[f64], low: &[f64], positions: &[usize]) -> Result<(), Box<dyn std::error::Error>> {
+fn plot(dates: &[&str], closing_prices: &[f64], high: &[f64], low: &[f64], positions: &[usize], min_price:f64, max_price:f64, stock_symbol:&str) -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::new("plot.png", (1024, 768)).into_drawing_area();
     root.fill(&WHITE)?;
 
@@ -40,7 +42,7 @@ fn plot(dates: &[&str], closing_prices: &[f64], high: &[f64], low: &[f64], posit
     let ymax = closing_prices.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
 
     let mut chart = ChartBuilder::on(&root)
-        .caption("Closing Prices", ("sans-serif", 50).into_font())
+        .caption(format!("{} Close Price", stock_symbol), ("sans-serif", 50).into_font())
         .margin(10)
         .x_label_area_size(30)
         .y_label_area_size(30)
@@ -55,6 +57,24 @@ fn plot(dates: &[&str], closing_prices: &[f64], high: &[f64], low: &[f64], posit
         (0..).zip(closing_prices.iter()).map(|(x, y)| (x, *y)),
         &RED,
     ))?;
+
+    //Annotate minimum
+    if let Some(min_index) = closing_prices.iter().position(|&p| p == min_price) {
+        chart.draw_series(std::iter::once(Text::new(
+            format!("Min: {:.2}", min_price),
+            (min_index, min_price),
+            ("sans-serif", 15).into_font(),
+        )))?;
+    }
+
+    // Annotate maximum
+    if let Some(max_index) = closing_prices.iter().position(|&p| p == max_price) {
+        chart.draw_series(std::iter::once(Text::new(
+            format!("Max: {:.2}", max_price),
+            (max_index, max_price),
+            ("sans-serif", 15).into_font(),
+        )))?;
+    }
 
     for &pos in positions {
         // Draw vertical line for error bar
@@ -88,4 +108,5 @@ fn plot(dates: &[&str], closing_prices: &[f64], high: &[f64], low: &[f64], posit
 
     Ok(())
 }
+
 
